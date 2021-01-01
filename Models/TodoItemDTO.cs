34e86482161da37f8adbc4a598ce4566b21d8a8e
Louis Chen <scu.louis@gmail.com>
2021-01-01 17:34:22 +0800

    add memory db

---
+++ Models/TodoItemDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TestWebApi.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using TestWebApi.Models;
 
 namespace TestWebApi
 {
@@ -25,6 +27,7 @@ namespace TestWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
